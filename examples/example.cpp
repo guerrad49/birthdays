ee5a9fda020d26f9871cf81f9e9f80728c5e2164
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2024-12-31 17:27:07 -0500

    wrote full example

--- examples/example.cpp
+++ examples/example.cpp
@@ -2,15 +2,24 @@
 #include <vector>
 
 #include "person.h"
+#include "book.h"
 
 int main() {
     using namespace Birthdays;
+    using Date = std::vector<uint16_t>;
 
-    Person p{"david", "guerra"};
-    std::cout << p.fullName() << std::endl;
-    
-    Person q{"marisela", "melendez"};
-    std::cout << q.fullName() << std::endl;
+    Book bb{
+        Person{"ozwald", "cobblepot", Date{1976,5,31}},
+        Person{"sophia", "falcone", Date{1985,8,16}},
+        Person{"victor", "aguilar", Date{1997,10,26}},
+        Person{"eve", "karlo", Date{1973,10,22}},
+        Person{"johnny", "viti", Date{1969,5,22}},
+        Person{"benny", "cobblepot", Date{2015,2,8}}
+    };
+
+    bb.update_ages(Date{2024,12,31});
+    bb.sort();
+    bb.display();
 
     return 0;
 }
