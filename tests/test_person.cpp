73e563199013268dd51ccdbacf25a3dd536fc523
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-01-04 09:20:27 -0500

    added tests for object comparison

--- tests/test_person.cpp
+++ tests/test_person.cpp
@@ -2,46 +2,62 @@
 
 #include "person.h"
 
+namespace testing {
+
 using namespace Birthdays;
 
 class PersonTest : public testing::Test {
 protected:
-    // PersonTest() {
-    //     // g_.setFirstName("George MiChael");
-    //     // g_.setLastName("bluTh");
-    // }
-
     Person p_;
-    Person* g_;
-    virtual void SetUp() override {
-        g_ = new Person("George MiChael", "bluTh");
-    }
+    Person g_;
 
-    // Person g_{"George MiChael", "bluTh"};
+    using Date = std::vector<uint16_t>;
+
+    PersonTest() : 
+        g_{"George MiChael", "bluTh", Date{1988,6,7}} {}
 };
 
-// Test default constructor.
-TEST_F(PersonTest, DefaultConstructor) {
+// Test constructors.
+TEST_F(PersonTest, ConstructorsTest) {
+    Date zero{0,0,0};
+
     EXPECT_TRUE(p_.firstName().empty());
-    std::vector<uint16_t> zero{0,0,0};
     EXPECT_EQ(p_.dob(), zero);
+
+    Person lb{"lindsay", "bluth"};
+    EXPECT_EQ(lb.fullName(), "lindsay bluth");
+    EXPECT_EQ(lb.dob(), zero);
+}
+    
+// Test formatting of names.
+TEST_F(PersonTest, NameFormatting) {
+    EXPECT_EQ(g_.firstName(), "george_michael");
+    EXPECT_EQ(g_.fullName(), "george_michael bluth");
 }
 
-// // Test formatting of names.
-// TEST_F(PersonTest, NameFormatting) {
-//     Person g{"George MiChael", "bluTh", std::vector<uint16_t>{1988,6,7}};
-//     EXPECT_EQ(g.fullName(), "george_michael bluth");
-// }
+// Test age.
+TEST_F(PersonTest, AgeTest) {
+    // Warning: Changing "today" changes test results.
+    Date today{2024,12,31};
 
-// // Test age.
-// TEST_F(PersonTest, AgeTest) {
-//     Person g{"George MiChael", "bluTh", std::vector<uint16_t>{1988,6,7}};
-//     std::vector<uint16_t> today{2024,12,27};
-//     g.setAge(today);
-//     EXPECT_EQ(g.age(), 36);
-//     EXPECT_EQ(p_.age(), 255);
+    EXPECT_EQ(p_.age(), 255);
+    p_.setDoB(Date{2024,1,1});
+    p_.setAge(today);
+    EXPECT_EQ(p_.age(), 0);
 
-//     Person baby("jane", "doe", std::vector<uint16_t>{2024,1,1});
-//     baby.setAge(today);
-//     EXPECT_EQ(baby.age(), 0);
-// }
+    g_.setAge(today);
+    EXPECT_EQ(g_.age(), 36);
+}
+
+// Test comparisons.
+TEST_F(PersonTest, ComparisonTest) {
+    Person gmb{"georGe michaeL", "Bluth", Date{1988,6,7}};
+    EXPECT_TRUE(g_ == gmb);
+
+    gmb.setDoB(Date{1988,6,6});
+    EXPECT_TRUE(g_ > gmb);
+    gmb.setDoB(Date{1988,6,8});
+    EXPECT_TRUE(g_ < gmb);
+}
+
+}  // End namespace testing.
