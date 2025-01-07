b91e5942f08387c1dbf13d9e0328f172245eab9b
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-01-07 15:23:53 -0500

    near complete person bindings

--- python/person_bindings.cpp
+++ python/person_bindings.cpp
@@ -1,33 +1,42 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-// #include <vector>
 
 #include "person.h"
 
 namespace py = pybind11;
+using namespace Birthdays;
 
-namespace Birthdays {
+namespace python {
 
-PYBIND11_MODULE(example, m) {
+PYBIND11_MODULE(Person, m) {
     py::class_<Person>(m, "Person")
+        // Constructors.
         .def(py::init<>())
-        .def(py::init<const std::string &, const std::string &>())
         .def(py::init<
             const std::string &, 
             const std::string &, 
             const std::vector<uint16_t> &
-            >())
+            >(),
+            py::arg("first") = "",
+            py::arg("last") = "",
+            py::arg("dob") = std::vector<uint16_t>{0,0,0}
+        )
+        
+        .def("__repr__",
+            [](const Person& p) {
+                std::ostringstream oss;
+                oss << p;
+                return oss.str();
+            }
+        )
+
         .def_property("firstName", &Person::firstName, &Person::setFirstName)
         .def_property("lastName", &Person::lastName, &Person::setLastName)
         .def_property("fullName", &Person::fullName, &Person::setFullName)
-        .def("dob", &Person::dob)
-        .def("age", &Person::age)
-        .def("dobStr", &Person::dobStr)
-        .def("__repr__",
-            [](Person& p) {
-                return "Person(" + p.firstName() + "," + p.lastName() + ")";
-            }
-        );
+        .def_property("dob", &Person::dob, &Person::setDoB)
+        .def_property("age", &Person::age, &Person::setAge)
+
+        .def("calcAge", &Person::calcAge);
     }
 
-} // End namespace Birthdays.
+} // End namespace python.
