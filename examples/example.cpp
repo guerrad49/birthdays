762e9584973921315d2ba288cf2f33afb5d7df9a
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-01-09 09:29:27 -0500

    changed spacing

--- examples/example.cpp
+++ examples/example.cpp
@@ -10,11 +10,11 @@ int main() {
 
     Book bb{
         Person{"ozwald", "cobblepot", Date{1976,5,31}},
-        Person{"sophia", "falcone", Date{1985,8,16}},
-        Person{"victor", "aguilar", Date{1997,10,26}},
-        Person{"eve", "karlo", Date{1973,10,22}},
-        Person{"johnny", "viti", Date{1969,5,22}},
-        Person{"benny", "cobblepot", Date{2015,2,8}}
+        Person{"sophia",   "falcone", Date{1985,8,16}},
+        Person{"victor",   "aguilar", Date{1997,10,26}},
+        Person{   "eve",     "karlo", Date{1973,10,22}},
+        Person{"johnny",      "viti", Date{1969,5,22}},
+        Person{ "benny", "cobblepot", Date{2015,2,8}}
     };
 
     bb.update_ages(Date{2024,12,31});
