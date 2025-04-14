88c9854affeef68538e92aeaf767bb071caf3ffa
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-04-14 16:01:26 -0400

    added tests of enum types and sort/filter methods

--- python/tests/test_book.py
+++ python/tests/test_book.py
@@ -1,7 +1,7 @@
 import unittest
 
-from Person import Person
-from Book import Book
+import pyBirthdays
+from pyBirthdays import Person, Book
 
 
 class BookTest(unittest.TestCase):
@@ -41,6 +41,7 @@ class BookTest(unittest.TestCase):
         p = Person("gob", "bluth", [1965,5,4])
         self.bb.remove(p)
         self.assertEqual(len(self.bb), startSize-1)
+        self.assertFalse(p in self.bb)
 
     def test_update_ages(self):
         defaultAges = [p.age for p in self.bb]
@@ -49,6 +50,58 @@ class BookTest(unittest.TestCase):
         self.bb.update_ages([2025,1,9])
         updatedAges = [p.age for p in self.bb]
         self.assertFalse( all(a == 255 for a in updatedAges) )
+
+    def test_enums(self):
+        self.assertEqual(
+            pyBirthdays.SORT_CALENDAR,
+            pyBirthdays.SORT_METHOD(1)
+        )
+        self.assertEqual(
+            pyBirthdays.SORT_LASTNAME,
+            pyBirthdays.SORT_METHOD(2)
+        )
+        self.assertEqual(
+            pyBirthdays.FILTER_MONTH,
+            pyBirthdays.FILTER_METHOD(1)
+        )
+        self.assertEqual(
+            pyBirthdays.FILTER_FIRSTNAME,
+            pyBirthdays.FILTER_METHOD(3)
+        )
+
+    def test_sort(self):
+        self.bb.sort(pyBirthdays.SORT_CALENDAR)
+        order = [8,1,3,6,2,7,4,5,0]
+        self.assertEqual(self.bb.ids, order)
+
+        self.bb.sort(pyBirthdays.SORT_LASTNAME)
+        order = [5,3,2,8,0,1,7,4,6]
+        self.assertEqual(self.bb.ids, order)
+
+        self.bb.update_ages([2024,12,31])
+        self.bb.sort(pyBirthdays.SORT_AGE)
+        order = [3,4,5,0,2,1,6,8,7]
+        self.assertEqual(self.bb.ids, order)
+
+        self.bb.sort(pyBirthdays.SORT_AGE, True)
+        reversed = order[::-1]
+        self.assertEqual(self.bb.ids, reversed)
+
+    def test_filter(self):
+        self.bb.filter(pyBirthdays.FILTER_MONTH, 9)
+        filtered = [4,5]
+        self.assertEqual(self.bb.ids, filtered)
+
+        # Reset to avoid additional filtering.
+        self.bb.reset_ids()
+        self.bb.filter(pyBirthdays.FILTER_LASTNAME, 'b')
+        filtered = [0,1,2,3,5,7,8]
+        self.assertEqual(self.bb.ids, filtered)
+
+        # Test additional filtering.
+        self.bb.filter(pyBirthdays.FILTER_FIRSTNAME, 'g')
+        filtered = [2,3,7]
+        self.assertEqual(self.bb.ids, filtered)
 
 
 if __name__ == "__main__":
