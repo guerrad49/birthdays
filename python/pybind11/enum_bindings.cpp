7f3f1d5b46b21a17788ef33a99a9d01f40bffa1e
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-04-14 15:37:34 -0400

    added enum bindings

---
+++ python/pybind11/enum_bindings.cpp
@@ -0,0 +1,27 @@
+#include <pybind11/pybind11.h>
+
+#include "book.h"
+
+
+namespace pybirthdays {
+
+namespace py = pybind11;
+using namespace birthdays;
+
+void init_enums_bind(py::module_ &m) {    
+    pybind11::enum_<SORT_METHOD>(m, "SORT_METHOD")
+        .value("SORT_CALENDAR", SORT_METHOD::CALENDAR)
+        .value("SORT_LASTNAME", SORT_METHOD::LASTNAME)
+        .value("SORT_AGE", SORT_METHOD::AGE)
+        .export_values()
+        ;
+
+    pybind11::enum_<FILTER_METHOD>(m, "FILTER_METHOD")
+        .value("FILTER_MONTH", FILTER_METHOD::MONTH)
+        .value("FILTER_LASTNAME", FILTER_METHOD::LASTNAME)
+        .value("FILTER_FIRSTNAME", FILTER_METHOD::FIRSTNAME)
+        .export_values()
+        ;
+}
+
+}  // End namespace pybirthdays.
