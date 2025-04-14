cdba285d61c76abb94ee2cfc56a597905d559a23
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-04-14 15:59:14 -0400

    switched order to define enum bindings before book bindings

--- python/pybind11/birthday_bindings.cpp
+++ python/pybind11/birthday_bindings.cpp
@@ -5,10 +5,12 @@ namespace pybirthdays {
 namespace py = pybind11;
 
 void init_person_bind(py::module_ &m);
+void init_enums_bind(py::module_ &m);
 void init_book_bind(py::module_ &m);
 
 PYBIND11_MODULE(pyBirthdays, m) {
     init_person_bind(m);
+    init_enums_bind(m);
     init_book_bind(m);
 }
 
--- python/pybind11/enum_bindings.cpp
+++ python/pybind11/enum_bindings.cpp
@@ -9,14 +9,14 @@ namespace py = pybind11;
 using namespace birthdays;
 
 void init_enums_bind(py::module_ &m) {    
-    pybind11::enum_<SORT_METHOD>(m, "SORT_METHOD")
+    py::enum_<SORT_METHOD>(m, "SORT_METHOD")
         .value("SORT_CALENDAR", SORT_METHOD::CALENDAR)
         .value("SORT_LASTNAME", SORT_METHOD::LASTNAME)
         .value("SORT_AGE", SORT_METHOD::AGE)
         .export_values()
         ;
 
-    pybind11::enum_<FILTER_METHOD>(m, "FILTER_METHOD")
+    py::enum_<FILTER_METHOD>(m, "FILTER_METHOD")
         .value("FILTER_MONTH", FILTER_METHOD::MONTH)
         .value("FILTER_LASTNAME", FILTER_METHOD::LASTNAME)
         .value("FILTER_FIRSTNAME", FILTER_METHOD::FIRSTNAME)
