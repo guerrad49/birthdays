b53da407869ef919ef3ce196127c44aec30653d3
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-04-18 19:40:29 -0400

    added dob test which checks validity of date

--- cpp/tests/test_person.cpp
+++ cpp/tests/test_person.cpp
@@ -12,37 +12,41 @@ protected:
     Person p_;
     Person g_;
 
-    using Date = std::vector<uint16_t>;
-
     PersonTest() : 
-        g_{"George MiChael", "bluTh", Date{1990,3,3}} {}
+        g_{"George MiChael", "bluTh", DateArray{1990,3,3}} {}
 };
 
-/// Test constructors.
-TEST_F(PersonTest, ConstructorsTest) {
-    Date zero{0,0,0};
-
-    EXPECT_TRUE(p_.firstName().empty());
-    EXPECT_EQ(p_.dob(), zero);
-
-    Person lb{"lindsay", "bluth"};
-    EXPECT_EQ(lb.fullName(), "lindsay bluth");
-    EXPECT_EQ(lb.dob(), zero);
-}
-    
 /// Test formatting of names.
 TEST_F(PersonTest, NameFormatting) {
     EXPECT_EQ(g_.firstName(), "george_michael");
     EXPECT_EQ(g_.fullName(), "george_michael bluth");
 }
 
+/// Test date-of-birth.
+TEST_F(PersonTest, DobTest) {
+    DateArray d1{0,0,0};
+    EXPECT_THROW(p_.setDoB(d1), std::domain_error);
+
+    DateArray d2{2025,1,32};
+    EXPECT_THROW(p_.setDoB(d2), std::domain_error);
+
+    DateArray d3{2025,2,29};
+    EXPECT_THROW(p_.setDoB(d3), std::domain_error);
+
+    DateArray d4{2025,13,1};
+    EXPECT_THROW(p_.setDoB(d4), std::domain_error);
+
+    DateArray d5{2024,2,29};  // Leap year.
+    EXPECT_NO_THROW(p_.setDoB(d5));
+}
+
 /// Test age.
 TEST_F(PersonTest, AgeTest) {
     // Warning: Changing date changes test results.
-    Date mockDate{2024,12,31};
+    DateArray mockDate{2024,12,31};
 
     EXPECT_EQ(p_.age(), 255);  // Default.
-    p_.setDoB(Date{2024,1,1});
+    p_.setDoB(DateArray{2024,1,1});
     p_.calcAge(mockDate);
     EXPECT_EQ(p_.age(), 0);
 
@@ -52,12 +56,12 @@ TEST_F(PersonTest, AgeTest) {
 
 /// Test comparisons.
 TEST_F(PersonTest, ComparisonTest) {
-    Person gmb{"georGe michaeL", "Bluth", Date{1990,3,3}};
+    Person gmb{"georGe michaeL", "Bluth", DateArray{1990,3,3}};
     EXPECT_TRUE(g_ == gmb);
 
-    gmb.setDoB(Date{1990,3,2});
+    gmb.setDoB(DateArray{1990,3,2});
     EXPECT_TRUE(g_ > gmb);
-    gmb.setDoB(Date{1990,3,4});
+    gmb.setDoB(DateArray{1990,3,4});
     EXPECT_TRUE(g_ < gmb);
 }
 
