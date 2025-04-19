505c9fa41aa8ce5e264c733921c99cb8087a0f99
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-04-19 08:02:54 -0400

    added more utilities to check for erroneous values

--- cpp/include/utils.hh
+++ cpp/include/utils.hh
@@ -2,12 +2,24 @@
 #define UTILS_HH
 
 #include <array>
+#include <string>
 #include <cstdint>
+#include <exception>
 
 
 namespace birthdays {
 
-bool is_valid_date(const std::array<uint16_t,3>& date) {
+class BirthdayError : public std::exception {
+public:
+    BirthdayError(const std::string& msg) : msg_(msg) {}
+    const char* what() const noexcept override {
+        return msg_.c_str();
+    }
+private:
+    std::string msg_;
+};
+
+inline bool is_valid_date(const std::array<uint16_t,3>& date) {
     uint16_t  year = date[0];
     uint16_t month = date[1];
     uint16_t   day = date[2];
@@ -23,6 +35,19 @@ bool is_valid_date(const std::array<uint16_t,3>& date) {
         daysInMonth[2] = 29;
 
     return day <= daysInMonth[month];
+}
+
+inline bool is_valid_month(char c) {
+    if (c < 1 || c > 12)
+        return false;
+    return true;
+}
+
+inline bool is_valid_letter(char c) {
+    unsigned char lowerCase = std::tolower(c);
+    if (lowerCase < 'a' || lowerCase > 'z')
+        return false;
+    return true;
 }
 
 }  // End namespace birthdays.
