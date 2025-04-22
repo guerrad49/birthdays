45aade993944d05f3cfaa8c46129d99fc94cf61b
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-04-22 17:54:57 -0400

    modified to be up-to-date

--- cpp/examples/example.cpp
+++ cpp/examples/example.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <vector>
 
-#include "person.h"
-#include "book.h"
+#include "person.hh"
+#include "book.hh"
 
 int main() {
     using namespace birthdays;
-    using Date = std::vector<uint16_t>;
 
     Book bb{
-        Person{"ozwald", "cobblepot", Date{1976,5,31}},
-        Person{"sophia",   "falcone", Date{1985,8,16}},
-        Person{"victor",   "aguilar", Date{1997,10,26}},
-        Person{   "eve",     "karlo", Date{1973,10,22}},
-        Person{"johnny",      "viti", Date{1969,5,22}},
-        Person{ "benny", "cobblepot", Date{2015,2,8}}
+        Person{"ozwald", "cobblepot", DateArray{1976,5,31}},
+        Person{"sophia",   "falcone", DateArray{1985,8,16}},
+        Person{"victor",   "aguilar", DateArray{1997,10,26}},
+        Person{   "eve",     "karlo", DateArray{1973,10,22}},
+        Person{"johnny",      "viti", DateArray{1969,5,22}},
+        Person{ "benny", "cobblepot", DateArray{2015,2,8}}
     };
 
     std::cout << "Book after initialization." << std::endl;
@@ -22,7 +21,7 @@ int main() {
 
     std::cout << std::endl;
     std::cout << "Update ages using 2024-12-31." << std::endl;
-    bb.update_ages(Date{2024,12,31});
+    bb.update_ages(DateArray{2024,12,31});
     bb.display();
 
     std::cout << std::endl;
