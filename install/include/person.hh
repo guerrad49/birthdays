5ca42213bc59f5a14a44b1a7a167e8a53fd65b81
guerrad49 <63264459+guerrad49@users.noreply.github.com>
2025-05-08 13:06:08 -0400

    updated installation

--- install/include/person.hh
+++ install/include/person.hh
@@ -19,8 +19,7 @@ class Person {
 public:
     friend class Book;
 
-    /// Default Constructor.
-    Person() = default;
+    Person() = default;  // Default constructor.
 
     /**
      * @brief Constructor.
@@ -35,46 +34,36 @@ public:
         const DateArray& dob
         );
     
-    /// Destructor.
-    ~Person() = default;
+    ~Person() = default;  // Default desctructor.
 
-    std::string firstName() const { return firstName_; }
-    std::string  lastName() const { return lastName_; }
-    std::string  fullName() const { return fullName_; }
+    std::string first_name() const { return firstName_; }
+    std::string  last_name() const { return lastName_; }
+    std::string  full_name() const { return fullName_; }
     DateArray dob() const { return dob_; }
     uint16_t age() const { return age_; }
-    /// Get date-of-birth as ISO-formatted string.
-    std::string dobStr() const;
+    /// @brief Get date-of-birth as ISO-formatted string.
+    std::string dob_str() const;
 
-    /**
-     * @brief Format names to lowercase and underscores.
-     * @param str The name string to format.
-     * @return The formatted name.
-     */
-    std::string formatName(const std::string& str);
-    void setFirstName(const std::string& str);
-    void setLastName(const std::string& str);
-    void setFullName();
-    void setDoB(const DateArray& v);
-    void setAge(const uint8_t& age) { age_ = age; }
+    
+    void set_first_name(const std::string& str);
+    void set_last_name(const std::string& str);
+    void set_full_name();
+    void set_dob(const DateArray& date);
+    void set_age(const uint8_t& age) { age_ = age; }
 
-    /**
-     * @brief Calculate and set age using today's date.
-     * @param tdy Today's date array.
-     */
-    void calcAge(const DateArray& tdy);
+    /// @brief Update age as of given date.
+    void set_age_as_of(const DateArray& date);
 
 protected:
-    /// Person string representation.
+    /// @brief Person string representation.
     friend std::ostream& operator<<(std::ostream& os, const Person& p);
 
-    /// Determine equality between two Person objects.
     friend bool operator==(const Person& lhs, const Person& rhs);
-    /// Compare using dob array comparison.
+    friend bool operator!=(const Person& lhs, const Person& rhs);
     friend bool operator<(const Person& lhs, const Person& rhs);
     friend bool operator>(const Person& lhs, const Person& rhs);
     friend bool operator<=(const Person& lhs, const Person& rhs);
-    friend bool operator<=(const Person& lhs, const Person& rhs);
+    friend bool operator>=(const Person& lhs, const Person& rhs);
 
 private:
     std::string firstName_;
@@ -82,6 +71,13 @@ private:
     std::string fullName_;
     DateArray dob_;
     uint8_t age_{255};  // Default is max.
+
+    /**
+     * @brief Format names to lowercase and underscores.
+     * @param str The name string to format.
+     * @return The formatted name.
+     */
+    std::string to_lowercase_(const std::string& str);
 };
 
 }  // End namespace birthdays.
